9044b94d709ad09a95be8f49d6b6703e339f1077
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2025-01-25 15:26:23 +0500

    fix UI

--- QuizGen/TelegramClient.cs
+++ QuizGen/TelegramClient.cs
@@ -14,11 +14,11 @@ namespace QuizGen
     internal static class TelegramClient
     {
         private static Td.Client? _client = null;
-        private readonly static Td.ClientResultHandler _defaultHandler = new DefaultHandler();
 
         private static TdApi.AuthorizationState? _authorizationState = null;
 
         public static Action<TdApi.AuthorizationState> OnAuthorizationStateChangedEvent;
+        public static Action<long, List<TdApi.KeyboardButton>> OnShowKeyboardEvent;
 
         static TelegramClient()
         {
@@ -34,14 +34,14 @@ namespace QuizGen
         public static void SendMessage(long chatId, string message)
         {
             TdApi.InputMessageContent content = new TdApi.InputMessageText(new TdApi.FormattedText(message, null), null, true);
-            _client.Send(new TdApi.SendMessage(chatId, 0, null, null, null, content), _defaultHandler);
+            _client.Send(new TdApi.SendMessage(chatId, 0, null, null, null, content), null);
         }
 
         public static void SendPoll(long chatId, Test test)
         {
             TdApi.InputMessagePoll poll = new TdApi.InputMessagePoll(test.Question, test.Answers.ToArray(), false, new TdApi.PollTypeQuiz(), 0, 0, false);
 
-            _client.Send(new TdApi.SendMessage(chatId, 0, null, null, null, poll), _defaultHandler);
+            _client.Send(new TdApi.SendMessage(chatId, 0, null, null, null, poll), null);
         }
 
         public static void SetPhoneNumber(string phoneNumber)
@@ -61,7 +61,7 @@ namespace QuizGen
 
         public static void LogOut()
         {
-            _client.Send(new TdApi.LogOut(), _defaultHandler);
+            _client.Send(new TdApi.LogOut(), null);
         }
 
         private static void OnAuthorizationStateUpdated(TdApi.AuthorizationState authorizationState)
@@ -91,14 +91,6 @@ namespace QuizGen
             }
         }
 
-        private class DefaultHandler : Td.ClientResultHandler
-        {
-            void Td.ClientResultHandler.OnResult(TdApi.BaseObject @object)
-            {
-                
-            }
-        }
-
         private class UpdateHandler : Td.ClientResultHandler
         {
             void Td.ClientResultHandler.OnResult(TdApi.BaseObject @object)
@@ -106,6 +98,25 @@ namespace QuizGen
                 if (@object is TdApi.UpdateAuthorizationState state)
                 {
                     OnAuthorizationStateUpdated(state.AuthorizationState);
+                }
+                if (@object is TdApi.UpdateNewMessage updateNewMessage)
+                {
+                    var message = updateNewMessage.Message;
+
+                    if (message.ReplyMarkup is TdApi.ReplyMarkupShowKeyboard showKeyboard)
+                    {
+                        var keyboard = new List<TdApi.KeyboardButton>();
+
+                        foreach(var row in showKeyboard.Rows)
+                        {
+                            foreach(var keyboardButton in row)
+                            {
+                                keyboard.Add(keyboardButton);
+                            }
+                        }
+
+                        OnShowKeyboardEvent?.Invoke(message.ChatId, keyboard);
+                    }
                 }
             }
         }
--- QuizGen/ViewModels/HomePageViewModel.cs
+++ QuizGen/ViewModels/HomePageViewModel.cs
@@ -3,13 +3,14 @@ using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NanoXLSX.Exceptions;
+using QuizGen.Models;
 using QuizGen.TestParsers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Telegram.Td.Api;
 using Td = Telegram.Td;
 using TdApi = Telegram.Td.Api;
 
@@ -17,6 +18,8 @@ namespace QuizGen.ViewModels
 {
     public partial class HomePageViewModel : ViewModelBase
     {
+        private const long quizbotId = 983000232;
+
         [ObservableProperty]
         private string? filePath;
         [ObservableProperty]
@@ -25,6 +28,8 @@ namespace QuizGen.ViewModels
         private string? testDescription;
 
         [ObservableProperty]
+        private int testRangeType;
+        [ObservableProperty]
         private int testRangeFrom;
         [ObservableProperty]
         private int testRangeTo;
@@ -32,7 +37,10 @@ namespace QuizGen.ViewModels
         private int testGrouping;
 
         [ObservableProperty]
-        private string? testInfo;
+        private int testDurationIndex;
+        [ObservableProperty]
+        private int testShufflingIndex;
+
         [ObservableProperty]
         private string? overallInfo;
 
@@ -41,17 +49,17 @@ namespace QuizGen.ViewModels
 
         public HomePageViewModel()
         {
-            
+            TelegramClient.OnShowKeyboardEvent += OnTelegramShowKeyboard;
         }
 
         [RelayCommand]
         private async Task OpenFile()
         {
-            if(TopLevel != null)
+            if (TopLevel != null)
             {
                 var storage = TopLevel.StorageProvider;
                 var defaultLocation = await storage.TryGetWellKnownFolderAsync(WellKnownFolder.Downloads);
-                var files = await storage.OpenFilePickerAsync(new FilePickerOpenOptions() 
+                var files = await storage.OpenFilePickerAsync(new FilePickerOpenOptions()
                 {
                     AllowMultiple = false,
                     SuggestedStartLocation = defaultLocation
@@ -66,21 +74,92 @@ namespace QuizGen.ViewModels
         {
             testParser = new LMSTestParser();
 
-            try 
+            try
             {
                 testParser.OpenFile(FilePath);
-                TestInfo = $"File loaded succesfully. Tests found: {testParser.GetTestCount()}";
+                OverallInfo += $"File loaded succesfully. Tests found: {testParser.GetTestCount()}\n";
             }
             catch (Exception e)
             {
-                TestInfo = e.InnerException != null ? e.InnerException.Message : e.Message;
+                OverallInfo += e.InnerException != null ? e.InnerException.Message : e.Message;
+                OverallInfo += "\n";
             }
         }
 
         [RelayCommand]
-        private void CreateTests()
+        private void Create()
+        {
+            if (TestRangeType == 0)
+            {
+                if (TestGrouping == 0)
+                {
+                    InitializeQuiz(string.Format(TestName, $"1 - {testParser.GetTestCount()}"));
+
+                    foreach (var test in testParser.GetAllTests())
+                    {
+                        TelegramClient.SendPoll(quizbotId, test);
+                    }
+
+                    TelegramClient.SendMessage(quizbotId, "/done");
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        [RelayCommand]
+        private void Clear()
         {
 
+        }
+
+        private void InitializeQuiz(string name)
+        {
+            TelegramClient.SendMessage(quizbotId, "/cancel");
+            TelegramClient.SendMessage(quizbotId, "/stop");
+
+            TelegramClient.SendMessage(quizbotId, "/newquiz");
+
+            if (string.IsNullOrEmpty(name))
+            {
+                OverallInfo += "Test name cannot be empty!\n";
+                return;
+            }
+            TelegramClient.SendMessage(quizbotId, TestName);
+            TelegramClient.SendMessage(quizbotId, string.IsNullOrEmpty(TestDescription) ? "/skip" : TestDescription);
+        }
+
+        private void CreateQuiz(string name, IEnumerable<Test> tests)
+        {
+            InitializeQuiz(name);
+
+            foreach (var test in tests)
+            {
+                TelegramClient.SendPoll(quizbotId, test);
+            }
+
+            TelegramClient.SendMessage(quizbotId, "/done");
+        }
+
+        private void OnTelegramShowKeyboard(long chatId, List<KeyboardButton> keyboard)
+        {
+            if(chatId == quizbotId)
+            {
+                if(keyboard.Count == 4)
+                {
+                    TelegramClient.SendMessage(quizbotId, keyboard[TestShufflingIndex].Text);
+                }
+                else
+                {
+                    TelegramClient.SendMessage(quizbotId, keyboard[TestDurationIndex].Text);
+                }
+            }
         }
     }
 }
