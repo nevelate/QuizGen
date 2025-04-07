dc278827878ad131a181ccdb0f639a09b58c7d06
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2025-04-07 18:50:52 +0500

    add WixSharpSetup project

---
+++ WixSharpSetup/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WixSharp;
+
+namespace WixSharpSetup
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var project = new Project("MyProduct",
+                              new Dir(@"%ProgramFiles%\My Company\My Product",
+                                  new File("Program.cs")));
+
+            project.GUID = new Guid("9eec5927-823a-4695-96a8-7d66eec07140");
+            //project.SourceBaseDir = "<input dir path>";
+            //project.OutDir = "<output dir path>";
+
+            project.BuildMsi();
+        }
+    }
+}
